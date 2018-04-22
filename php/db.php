a03cefa38b4b0dbbb3a3b54d32eff9fceb8ff230
Nastia Pavliuk <nas.pa1997@ukr.net>
2018-04-23 01:52:22 +0300

    db connect

--- php/db.php
+++ php/db.php
@@ -1,6 +1,6 @@
 <?php
-$link = "localhost";
-$username = "root";
-$password = "mysql";
-$dbname = "course";
+$link = "eu-cdbr-west-02.cleardb.net";
+$username = "b214081257480f";
+$password = "7a6b9363";
+$dbname = "heroku_23c5873e8af4f49";
 ?>
